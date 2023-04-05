6fe9547490226abb58e9916fdba37e7d78ad7945
Krzysztof <switek.k@gmail.com>
2023-04-05 09:27:21 +0200

    Hello world

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is Krzysztof");
+        System.out.println("I'm 47 years old");
+        System.out.println("First letter of my surname is 'Ś'");
+    }
+}
