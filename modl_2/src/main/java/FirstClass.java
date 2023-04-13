096e4c9742680211c535d8f27280ddc2e6c9c23b
Krzysztof <switek.k@gmail.com>
2023-04-13 13:50:39 +0200

    firstClass and Notebook - module 2

--- modl_2/src/main/java/FirstClass.java
+++ modl_2/src/main/java/FirstClass.java
@@ -1,19 +1,34 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook(600,100, 2000);             // w tym miejscu tworzę obiekt?
-        Notebook heavyNotebook = new Notebook( 2000, 200, 2023);
-        Notebook oldNotebook = new Notebook( 1200, 300, 2010);
+        Notebook notebook = new Notebook(600,500, 2000);             // w tym miejscu tworzę obiekt?
+        Notebook heavyNotebook = new Notebook( 2000, 1000, 2023);
+        Notebook oldNotebook = new Notebook( 1400, 700, 2010);
 
-        System.out.println("Notebook weight: " + notebook.weight + " Notebook price: " + notebook.weight);
-        notebook.checkPrice();                                          // wywołuję metodę klasy
-        System.out.println("Notebook weight: " + heavyNotebook.weight + " Notebook price: " + heavyNotebook.weight);
-        System.out.println("Notebook weight: " + oldNotebook.weight + " Notebook price: " + oldNotebook.weight);
+        System.out.println("Notebook weight: " + notebook.weight +";"+ " Notebook price: " + notebook.price +";"+ " Production year: " + notebook.year);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkValue4Money();
+        System.out.println("Notebook weight: " + heavyNotebook.weight +";"+ " Notebook price: " + heavyNotebook.price +";"+ " Production year: " + heavyNotebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkValue4Money();
+        System.out.println("Notebook weight: " + oldNotebook.weight +";"+ " Notebook price: " + oldNotebook.price +";"+ " Production year: " + oldNotebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        heavyNotebook.checkValue4Money();
+
+
+
+
+
+        /*
+
 
         if (notebook.price < 600) {                                 // sprawdzam pojedyńczy obiekt w Klasie
             System.out.println("This notebook is cheep ");
         } else {
             System.out.println("This notebook is quite expensive");
         }
-
+*/
     }
 }
--- modl_2/src/main/java/Notebook.java
+++ modl_2/src/main/java/Notebook.java
@@ -11,13 +11,44 @@ public class Notebook {
     }
 
     // metoda klasy
-    public void checkPrice() {
-        if (this.price < 900) {
-            System.out.println("This notebook is cheap.");
-        } else {
-            System.out.println("This notebook is quite expensive.");
-        }
 
+    /*  Zmodyfikuj metodę checkPrice w taki sposób, by sprawdzała kilka przedziałów cenowych:
+        jeśli cena jest niższa od 600, wyświetl "This notebook is very cheap.",
+        jeśli cena jest z przedziału 600-1000, wyświetl "The price is good.",
+        jeśli cena jest wyższa od 1000, wyświetl "This notebook is expensive.".
+     */
+
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap.");
+        } else if (this.price > 600 & this.price <= 1000){
+            System.out.println("The price is good.");
+        } else {System.out.println("This notebook is expensive.");
+        }
     }
 
+    /*  Dodaj nową metodę checkWeight, która będzie sprawdzać wagę urządzenia.
+        Samodzielnie napisz instrukcję warunkową i dobierz przedziały wagowe:
+        w zależności od podanej wagi ma wyświetlać się wiadomość,
+        że dane urządzenie jest lekkie, niezbyt ciężkie i bardzo ciężkie.
+     */
+    public void checkWeight() {
+        if (this.weight < 1000) {
+            System.out.println("This notebook is very light.");
+        } else if (this.weight > 1000 & this.weight <= 1500){
+            System.out.println("This notebook is of normal weight.");
+        } else System.out.println("This notebook is rather heavy.");
+        }
+
+        /* Dodaj instrukcję warunkową (z co najmniej jednym else if), która będzie sprawdzać zarówno rok produkcji,
+        jak i cenę. Samodzielnie dobierz zakresy i napisz komunikaty. Przykładowo, laptop o cenie powyżej 1500,
+        wyprodukowany później niż w roku 2018, otrzyma inny komunikat, niż urządzenie z roku 2000 o cenie 600.
+         */
+        public void checkValue4Money() {
+            if (this.year > 2000 & this.price < 1200) {
+                System.out.println("That ia a good value for money");
+            } else if (this.year >= 2010 & this.price < 800){
+                System.out.println("That is cheap but still acceptable");
+            } else System.out.println("Do not be bothered - it is a junk");
+        }
 }
