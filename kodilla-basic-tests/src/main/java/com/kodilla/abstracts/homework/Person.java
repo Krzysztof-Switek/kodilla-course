e85da719f1211a769acbe61bc26ba8726884ed30
Krzysztof Switek <switek.k@gmail.com>
2023-05-26 20:58:39 +0200

    3.2. Klasy abstrakcyjne (hermetyzacja atrybutów klasy Person)

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
@@ -2,40 +2,54 @@ package com.kodilla.abstracts.homework;
 
 public class Person {
 
-    String name;
-    String job;
-    int age;
-    public Person(String name, String job, int age) {
+    private String name;
+    private Job job;
+    private int age;
+
+    public Person(String name, Job job, int age) {
         this.name = name;
         this.job = job;
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     public static void main(String[] args) {
-        Person person = new Person("Andy","Project manager",32);
+        Person person = new Person("Andy", new ProjectManager("", 0), 32);
 
-        Job projectManager = new ProjectManager("",0);
+        Job projectManager = new ProjectManager("", 0);
         Job scrumMaster = new ScrumMaster("", 0);
-        Job javadeveloper = new JavaDeveloper("", 0);
+        Job javaDeveloper = new JavaDeveloper("", 0);
 
-        if (person.job.equals("Project manager")) {
-            System.out.println("First name: "  + person.name + "\nAge:" + person.age + "\nJob: " + person.job);
-            System.out.println("Responsibilities: " + projectManager.responsibilities());
-            System.out.println("Salary:"+ projectManager.salary());
+        if (person.getJob() instanceof ProjectManager) {
+            System.out.println("First name: " + person.getName() + "\nAge: " + person.getAge() + "\nJob: " + person.getJob().getTitle());
+            System.out.println("Responsibilities: " + person.getJob().responsibilities());
+            System.out.println("Salary: " + person.getJob().salary());
 
-        } else if (person.job.equals("Scrum master")) {
-            System.out.println("First name: "  + person.name + "\nAge:" + person.age + "\nJob: " + person.job);
-            System.out.println("Responsibilities: " + scrumMaster.responsibilities());
-            System.out.println("Salary: "+ scrumMaster.salary());
+        } else if (person.getJob() instanceof ScrumMaster) {
+            System.out.println("First name: " + person.getName() + "\nAge: " + person.getAge() + "\nJob: " + person.getJob().getTitle());
+            System.out.println("Responsibilities: " + person.getJob().responsibilities());
+            System.out.println("Salary: " + person.getJob().salary());
 
-        }else if (person.job.equals("Java developer")) {
-            System.out.println("First name: "  + person.name + "\nAge:" + person.age + "\nJob: " + person.job);
-            System.out.println("Responsibilities: " + javadeveloper.responsibilities());
-            System.out.println("Salary: "+ javadeveloper.salary());
+        } else if (person.getJob() instanceof JavaDeveloper) {
+            System.out.println("First name: " + person.getName() + "\nAge: " + person.getAge() + "\nJob: " + person.getJob().getTitle());
+            System.out.println("Responsibilities: " + person.getJob().responsibilities());
+            System.out.println("Salary: " + person.getJob().salary());
 
-        }else {
-            System.out.println("Please choose a job from the list :\n'Project manager'\n'Scrum master' \n'Java developer'");
+        } else {
+            System.out.println("Please choose a job from the list:\n'Project manager'\n'Scrum master'\n'Java developer'");
         }
     }
 }
+
 
