7954bf9733df985283d64739892005e28661017c
Krzysztof Switek <switek.k@gmail.com>
2023-05-28 17:10:08 +0200

    5.3 Klasy niemutowalne

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,25 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+import com.kodilla.collections.adv.immutable.BookRecord;
+
+public class Task {
+    protected String title;
+    protected int duration;
+    public Task (String title, int duration){
+        this.duration = duration;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public static void main(String[] args) {
+        TaskRecord taskRecord= new TaskRecord("Task_title", 77);
+    }
+}
+
