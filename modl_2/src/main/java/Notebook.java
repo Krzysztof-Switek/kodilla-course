13e0bb92c7810df948dc1f59bfb1faf3f44eefd8
Krzysztof Switek <switek.k@gmail.com>
2023-06-01 18:02:27 +0200

    2.3 - poprawiony kod oraz usunięte komentarze

--- modl_2/src/main/java/Notebook.java
+++ modl_2/src/main/java/Notebook.java
@@ -1,6 +1,5 @@
 public class Notebook {
 
-    //construktor
     int weight;
     int price;
     int year;
@@ -10,26 +9,15 @@ public class Notebook {
         this.year = year;
     }
 
-     /*  Zmodyfikuj metodę checkPrice w taki sposób, by sprawdzała kilka przedziałów cenowych:
-        jeśli cena jest niższa od 600, wyświetl "This notebook is very cheap.",
-        jeśli cena jest z przedziału 600-1000, wyświetl "The price is good.",
-        jeśli cena jest wyższa od 1000, wyświetl "This notebook is expensive.".
-     */
-
     public void checkPrice() {
-        if (this.price <= 600) {
+        if (this.price < 600) {
             System.out.println("This notebook is very cheap.");
-        } else if (this.price > 600 && this.price <= 1000){
+        } else if (this.price => 600 && this.price <= 1000){
             System.out.println("The price is good.");
         } else {System.out.println("This notebook is expensive.");
         }
     }
 
-    /*  Dodaj nową metodę checkWeight, która będzie sprawdzać wagę urządzenia.
-        Samodzielnie napisz instrukcję warunkową i dobierz przedziały wagowe:
-        w zależności od podanej wagi ma wyświetlać się wiadomość,
-        że dane urządzenie jest lekkie, niezbyt ciężkie i bardzo ciężkie.
-     */
     public void checkWeight() {
         if (this.weight <= 1000) {
             System.out.println("This notebook is very light.");
@@ -38,10 +26,6 @@ public class Notebook {
         } else System.out.println("This notebook is rather heavy.");
         }
 
-        /* Dodaj instrukcję warunkową (z co najmniej jednym else if), która będzie sprawdzać zarówno rok produkcji,
-        jak i cenę. Samodzielnie dobierz zakresy i napisz komunikaty. Przykładowo, laptop o cenie powyżej 1500,
-        wyprodukowany później niż w roku 2018, otrzyma inny komunikat, niż urządzenie z roku 2000 o cenie 600.
-         */
         public void checkValue4Money() {
             if (this.year > 2000 && this.price < 1200) {
                 System.out.println("That ia a good value for money");
