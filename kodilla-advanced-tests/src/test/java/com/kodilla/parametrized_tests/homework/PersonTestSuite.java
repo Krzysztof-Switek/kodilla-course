706124f1dd07b127d9db7849f49241ba5a46ef86
Krzysztof Switek <switek.k@gmail.com>
2023-07-05 17:41:33 +0200

    7.3. Obiekty jako dane wejściowe - homework - wszystkie wartości

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -19,11 +19,17 @@ public class PersonTestSuite {
 
     private static Stream<Arguments> giveHeightWeightData() {
         return Stream.of(
-                Arguments.of(1.75, 70, "Normal (healthy weight)"),
+                Arguments.of(1.8, 45, "Very severely underweight"),
+                Arguments.of(1.7, 44, "Severely underweight"),
                 Arguments.of(1.6, 45, "Underweight"),
+                Arguments.of(1.75, 70, "Normal (healthy weight)"),
                 Arguments.of(1.8, 90, "Overweight"),
+                Arguments.of(1.65, 90, "Obese Class I (Moderately obese)"),
                 Arguments.of(1.65, 100, "Obese Class II (Severely obese)"),
-                Arguments.of(1.9, 200, "Obese Class V (Super Obese)")
+                Arguments.of(1.85, 140, "Obese Class III (Very severely obese)"),
+                Arguments.of(1.75, 150, "Obese Class IV (Morbidly Obese)"),
+                Arguments.of(1.9, 200, "Obese Class V (Super Obese)"),
+                Arguments.of(1.9, 220, "Obese Class VI (Hyper Obese)")
         );
     }
 }
