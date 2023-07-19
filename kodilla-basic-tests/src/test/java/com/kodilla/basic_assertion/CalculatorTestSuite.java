4a90ce6c98c2d0019e963926c804f7d965a551f0
Krzysztof Switek <switek.k@gmail.com>
2023-07-19 14:10:32 +0200

    3.6 - calkulator test suite - poprawiony

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -8,33 +8,44 @@ public class CalculatorTestSuite {
 
     @Test
     public void testSum() {
+        // Given
         Calculator calculator = new Calculator();
         int a = 5;
         int b = 8;
+
+        // When
         double sumResult = calculator.sum(a, b);
-        assertEquals(13, sumResult);
+
+        // Then
+        assertEquals(13, sumResult, 0.01);
     }
 
     @Test
     public void testSubstract() {
+        // Given
         Calculator calculator = new Calculator();
         int a = 10;
         int b = 5;
+
+        // When
         double substractResult = calculator.substract(a, b);
-        assertEquals(5, substractResult);
+
+        // Then
+        assertEquals(5, substractResult, 0.01);
     }
 
     @Test
     public void testPower() {
+        // Given
         Calculator calculator = new Calculator();
         double a = -5;
         double c = 4;
+
+        // When
         double powerResult = calculator.power(a, c);
+
+        // Then
         assertEquals(625, powerResult, 0.01);
     }
-
-
-
-
-
 }
+
